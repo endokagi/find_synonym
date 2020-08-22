facb79506248da6b9d6a87307985f4ac06676674
Witthawat Tanoi <thewlip2008@gmail.com>
2020-08-22 18:33:34 +0700

    added pythainlp synonym

---
+++ synonyms_pythai.py
@@ -0,0 +1,33 @@
+from pythainlp.corpus.wordnet import lemmas
+import reader_writer_csv as rw
+from progress.bar import IncrementalBar
+
+def find_synonyms(path_folder,namefile):
+    word = rw.get_data(f'./{path_folder}/{namefile}.csv')
+    max = len(word)
+    bar = IncrementalBar(f'Progress', max=max,
+                         suffix='%(percent)d%% %(elapsed_td)s')
+    for row in word:
+        trans_word = transform(row["word"])
+        try:
+            find_synonyms = lemmas(trans_word)
+            row.update({"synonyms":find_synonyms})
+        except:
+            pass
+        bar.next()
+    bar.finish()   
+    fieldnames = ['word','synonyms','tf-idf-pos','tf-idf-neg','tf-idf-val','node-label']
+    rw.write_data_by_columns(f"./synonyms/pythai/{path_folder}/{namefile}.csv", fieldnames, word)
+
+def transform(word):
+    word = word.replace('(*', '')
+    word = word.replace('(', '')
+    word = word.replace(' - VERB)', '')
+    word = word.replace(' - ADV)', '')
+    word = word.replace(' - ADJ)', '')
+    return word
+
+def main():
+    find_synonyms('max_range_3','tfidf_p95_t7')
+    
+main()
